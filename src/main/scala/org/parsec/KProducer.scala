0cfda10557b741175a80b3aaf96ffa43740e1790
Josep Casals <joscas@gmail.com>
2016-12-12 18:36:22 +0000

    Simple docker alternative for simulation

--- src/main/scala/org/parsec/KProducer.scala
+++ src/main/scala/org/parsec/KProducer.scala
@@ -12,10 +12,10 @@ import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, Produce
 class KProducer[K <: Product, V <: Product] {
 
   val kafkaProps = new Properties()
-  kafkaProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "cloudera.landoop.com:29092")
+  kafkaProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
   kafkaProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[KafkaAvroSerializer].getCanonicalName)
   kafkaProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[KafkaAvroSerializer].getCanonicalName)
-  kafkaProps.put("schema.registry.url", "http://cloudera.landoop.com:28081")
+  kafkaProps.put("schema.registry.url", "http://localhost:8081")
   private lazy val producer  = new KafkaProducer[GenericRecord, GenericRecord](kafkaProps)
 
   def produce(topic: String, key: K, value: V, partition: Int = 0)(implicit toRecordKey: ToRecord[K], fromRecordKey: FromRecord[K], toRecord: ToRecord[V], fromRecord: FromRecord[V]): Future[RecordMetadata] = {
