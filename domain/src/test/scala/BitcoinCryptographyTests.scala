9b60e84331e3bfb001d5583faf82fbfaafeb398b
Josep Casals <joscas@gmail.com>
2016-12-21 13:17:55 +0000

    Test traits directly

---
+++ domain/src/test/scala/BitcoinCryptographyTests.scala
@@ -0,0 +1,7 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+class BitcoinCryptographyTests extends FlatSpec with Matchers{
+  "Bitcoin P2PKH addresses" should "be derived correctly from public keys"
+
+  "Bitcoin P2SH addresses" should "be derived correctly from public keys"
+}
