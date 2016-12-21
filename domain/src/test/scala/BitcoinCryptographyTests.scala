9ad91b8f1726d502ef8d3dc36f1fd326cd33d178
Josep Casals <joscas@gmail.com>
2016-12-21 20:49:53 +0000

    Can derive compressed addrresses from compressed public keys for Bitcoin

--- domain/src/test/scala/BitcoinCryptographyTests.scala
+++ domain/src/test/scala/BitcoinCryptographyTests.scala
@@ -3,10 +3,10 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class BitcoinCryptographyTests extends FlatSpec with Matchers with BitcoinCryptography {
 
-  "Bitcoin addresses" should "be derived correctly from public keys" in {
-    val publicKey = "bb48ae3726c5737344a54b3463fec499cb108a7d11ba137ba3c7d043bd6d7e14994f60462a3f91550749bb2ae5411f22b7f9bee79956a463c308ad508f3557df"
-    val address = "89b44e4d3c81ede05d0f5de8d1a68f754d73d997"
+  "Bitcoin addresses" should "be derived correctly from compressed public keys" in {
+    val compressedPublicKey = "026591ed35e4a6c43684ede7b2cb37f6cc19fc6629a2abce8ee0f3e021e3f274e6"
+    val compressedAddress = "1DipSsz4YYnjufGqVkHcbjrJEyHnxsY3Ec"
 
-    bitcoinAddressFromPublicKey(publicKey) should be (address)
+    bitcoinAddressFromPublicKey(compressedPublicKey) should be (compressedAddress)
   }
 }
