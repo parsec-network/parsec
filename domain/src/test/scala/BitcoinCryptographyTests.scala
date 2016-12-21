ba0a3e0808afb722c190053d13e757a4cae7e54b
Josep Casals <joscas@gmail.com>
2016-12-21 20:52:07 +0000

    Uncompressed bitcoin addresses

--- domain/src/test/scala/BitcoinCryptographyTests.scala
+++ domain/src/test/scala/BitcoinCryptographyTests.scala
@@ -9,4 +9,11 @@ class BitcoinCryptographyTests extends FlatSpec with Matchers with BitcoinCrypto
 
     bitcoinAddressFromPublicKey(compressedPublicKey) should be (compressedAddress)
   }
+
+  "Bitcoin addresses" should "be derived correctly from uncompressed public keys" in {
+    val unCompressedPublicKey = "046591ed35e4a6c43684ede7b2cb37f6cc19fc6629a2abce8ee0f3e021e3f274e6013ba122194117982eb818322f3e3b12a7edb15d586cdff87714de281c4ce5ec"
+    val unCompressedAddress = "19ZE9JY8NjX569nXGeHKwHzuqmaVvu3WuT"
+
+    bitcoinAddressFromPublicKey(unCompressedPublicKey) should be (unCompressedAddress)
+  }
 }
