005a3bf822fe9a505b3c5d469e6af5f2ed4f90c6
Josep Casals <joscas@gmail.com>
2016-12-21 21:23:02 +0000

    Bitcoin addresses complete

--- domain/src/main/scala/BitcoinCryptography.scala
+++ domain/src/main/scala/BitcoinCryptography.scala
@@ -4,16 +4,6 @@ import org.apache.commons.codec.binary.Hex
 import org.bitcoinj.core.{ECKey, NetworkParameters}
 
 trait BitcoinCryptography {
-  // TODO: implement me
-  def bitcoinP2PKHAddressFromPublicKey(key: String): String = {
-    "btc-public-key"
-  }
-
-  // TODO: implement me
-  def bitcoinP2SHAddressFromPublicKey(key: String): String = {
-    "btc-public-key"
-  }
-
   def bitcoinAddressFromPublicKey(key: String): String = {
     val ECkey = ECKey.fromPublicOnly(Hex.decodeHex(key.toCharArray))
     val netParams = NetworkParameters.prodNet() // Generating only production addresses
@@ -21,56 +11,3 @@ trait BitcoinCryptography {
     addressFromKey.toString
   }
 }
-
-// TODO: DELETE ME Example implementation
-
-//>  ECKey key =
-//>         ECKey.fromPrivate(HashUtil.sha3(TimeStamp+myuniquepass+randomnumber .getBytes())); // not working code , but you get the point.
-//>
-//>
-//>         byte[] addr = key.getAddress();
-//>         byte[] priv = key.getPrivKeyBytes();
-//>           byte[] pb=      key.getPubKey();
-//>
-//>         String addrBase16 = Hex.toHexString(addr);
-//>         String privBase16 = Hex.toHexString(priv);
-//
-//001|package com.waferthin.bitcoinj;
-//002|
-//003|import com.google.bitcoin.core.ECKey;
-//004|import com.google.bitcoin.core.NetworkParameters;
-//005|import com.google.bitcoin.core.Address;
-//006|
-//007|public class CreateAddress {
-//  008|
-//  009|    public static void main(String[] args) throws Exception {
-//    010|
-//      011|        // use test net by default
-//      012|        String net = "test";
-//    013|
-//      014|        if (args.length >= 1 && (args[0].equals("test") || args[0].equals("prod"))) {
-//      015|            net = args[0];
-//      016|            System.out.println("Using " + net + " network.");
-//      017|        }
-//    018|
-//      019|        // create a new EC Key ...
-//      020|        ECKey key = new ECKey();
-//    021|
-//      022|        // ... and look at the key pair
-//      023|        System.out.println("We created key:\n" + key);
-//    024|
-//      025|        // either test or production net are possible
-//      026|        final NetworkParameters netParams;
-//    027|
-//      028|        if (net.equals("prod")) {
-//      029|            netParams = NetworkParameters.prodNet();
-//      030|        } else {
-//      031|            netParams = NetworkParameters.testNet();
-//      032|        }
-//    033|
-//      034|        // get valid Bitcoin address from public key
-//      035|        Address addressFromKey = key.toAddress(netParams);
-//    036|
-//      037|        System.out.println("On the " + net + " network, we can use this address:\n" + addressFromKey);
-//    038|    }
-//  039|}
