28fc25b6e0012ce47f3b433a2d262b8f4b902a1e
pauloglesby <paul.oglesby@gmail.com>
2017-01-18 00:40:33 +0000

    Update `cats` version.

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
 
   val akkaVersion = "2.4.16"
   val akkaHttpVersion = "10.0.1"
-  val catsVersion = "0.4.1"
+  val catsVersion = "0.9.0"
   val confluentVersion = "3.0.1"
   val kafkaVersion = "0.10.0.0"
   val slf4jVersion = "1.7.7"
